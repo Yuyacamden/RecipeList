bfb13b01e5f8aaf4d3f4d765fe6d49f54a2e8f3c
Yuyacamden <82131357+Yuyacamden@users.noreply.github.com>
2022-06-29 16:37:58 +0900

    Initial Commit

---
+++ RecipeList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeList
+//
+//  Created by Yuya on 2022/06/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeList/RecipeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListApp.swift
+//  RecipeList
+//
+//  Created by Yuya on 2022/06/29.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
